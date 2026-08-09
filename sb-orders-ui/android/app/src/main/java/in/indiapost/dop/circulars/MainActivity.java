7d7502a986343594c5ab8e7a4daceda7081e4d63
sudipcemk40 <sudipkumar.mitra@indiapost.gov.in>
2026-08-09 11:59:39 +0530

    added pdf.go

---
+++ sb-orders-ui/android/app/src/main/java/in/indiapost/dop/circulars/MainActivity.java
@@ -0,0 +1,5 @@
+package in.indiapost.dop.circulars;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ sb-orders-ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.indiapost.dop.circulars',
+  appName: 'DOP Circulars and Orders',
+  webDir: 'build'
+};
+
+export default config;
